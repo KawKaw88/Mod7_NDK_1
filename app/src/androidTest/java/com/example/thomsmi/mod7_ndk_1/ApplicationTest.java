86082c261668fcef4bd18c7789030641df3b7d0c
KawKaw88 <tjs24suburbia@gmail.com>
2015-12-19 12:28:53 -0600

    Initial commit

---
+++ app/src/androidTest/java/com/example/thomsmi/mod7_ndk_1/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.thomsmi.mod7_ndk_1;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
